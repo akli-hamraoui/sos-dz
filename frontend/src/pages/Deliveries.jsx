9673ef52a32331bef1ac29d88deaf3472e80d1cc
Akli HAMRAOUI <hamraoui.akli@gmail.com>
2026-09-08 12:27:41 +0200

    perf: center map on points without animation

--- frontend/src/pages/Deliveries.jsx
+++ frontend/src/pages/Deliveries.jsx
@@ -271,13 +271,13 @@ export default function Deliveries() {
           // markers themselves (see fitView below), which can leave the
           // destination well outside the visible area once a trajectory is
           // drawn -- zoom out just enough to fit both ends of the line.
-          map.fitBounds(L.latLngBounds([from, dest]).pad(0.3), { maxZoom: 13 })
+          map.fitBounds(L.latLngBounds([from, dest]).pad(0.3), { maxZoom: 13, animate: false })
           fetchDrivingRoute(from, dest)
             .then((route) => {
               if (routeLineRef.current !== straight) return // superseded by another click/re-render meanwhile
               map.removeLayer(straight)
               routeLineRef.current = L.polyline(route.coordinates, { color: ROUTE_COLOR, weight: 4, dashArray: '1,10', lineCap: 'round' }).addTo(map)
-              map.fitBounds(L.latLngBounds(route.coordinates).pad(0.3), { maxZoom: 13 })
+              map.fitBounds(L.latLngBounds(route.coordinates).pad(0.3), { maxZoom: 13, animate: false })
             })
             .catch(() => {
               /* routing service unreachable -- the basic straight line drawn above stays as-is */
@@ -287,7 +287,7 @@ export default function Deliveries() {
       })
       if (fitView) {
         if (locationsToRender.length > 1) {
-          map.fitBounds(L.latLngBounds(locationsToRender.map((l) => [l.latitude, l.longitude])).pad(0.3), { maxZoom: 13 })
+          map.fitBounds(L.latLngBounds(locationsToRender.map((l) => [l.latitude, l.longitude])).pad(0.3), { maxZoom: 13, animate: false })
         } else if (locationsToRender.length === 1) {
           map.setView([locationsToRender[0].latitude, locationsToRender[0].longitude], 13)
         } else {
