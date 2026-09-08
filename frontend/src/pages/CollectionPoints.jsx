c193435547df0e24ffb2c84272ad38aac30b17d9
Akli HAMRAOUI <hamraoui.akli@gmail.com>
2026-09-08 09:58:08 +0200

    style: soften collection green accents

--- frontend/src/pages/CollectionPoints.jsx
+++ frontend/src/pages/CollectionPoints.jsx
@@ -336,7 +336,7 @@ export default function CollectionPoints() {
           const icon = L.divIcon({
             className: 'cp-marker-icon',
             html:
-              '<span class="cp-marker-pin"><svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="#2f6b52" stroke-width="2" ' +
+              '<span class="cp-marker-pin"><svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="#2c8f67" stroke-width="2" ' +
               'stroke-linecap="round" stroke-linejoin="round"><path d="M3.5 7.5 12 3l8.5 4.5v9L12 21l-8.5-4.5v-9Z"/>' +
               '<path d="M3.5 7.5 12 12l8.5-4.5"/><path d="M12 12v9"/></svg></span>',
             iconSize: [30, 30],
